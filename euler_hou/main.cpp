b859f343a78ff748d1b935c99cf16bc092cc03c4
山口勉 <yamaguchitsutomu@yamaguchitsutomunomacbook-puro.local>
2018-09-01 21:42:08 +0900

    Initial Commit

---
+++ euler_hou/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  euler_hou
+//
+//  Created by 山口勉 on 2018/09/01.
+//  Copyright © 2018年 山口勉. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
